dd08246d65dcfaed43328e6c2990e2acb273d265
Grygorii Nakonechnyi <grygorii.nakonechnyi@eleks.com>
2018-04-03 19:05:56 +0300

    update stylese and markup; simplify scripts;

--- main.js
+++ main.js
@@ -1,31 +1,18 @@
-
 var mainModule = (function () {
 
     var domElements;
     window.onload = function () {
         domElements = {
-            expandMenu: document.getElementsByClassName("expand-menu")[0],
-            headerTitle: document.getElementsByClassName("title-img")[0],
-            menuButton: document.getElementsByClassName("drop-btn")[0],
-            headerBlock: document.getElementsByClassName("header-block")[0],
-            contentBlock: document.getElementsByClassName("content")[0],
-            navigationList: document.getElementsByClassName("expand-menu")[0],
-            navigationItems: document.getElementsByClassName("list-item"),
+            expandMenu: document.getElementsByClassName("expand-menu")[0]
         };
     };
 
     function openMenu() {
-        domElements.headerTitle.setAttribute("src", "images/logo/logo-rules-white.svg");
-        domElements.headerBlock.classList.add('header-block-open-menu');
         domElements.expandMenu.classList.add('expand-menu-open');
-        domElements.menuButton.classList.add('drop-btn-open');
     };
 
     function closeMenu() {
-        domElements.headerTitle.setAttribute("src", "images/logo/logo-rules-dark.svg");
-        domElements.headerBlock.classList.remove('header-block-open-menu');
         domElements.expandMenu.classList.remove('expand-menu-open');
-        domElements.menuButton.classList.remove('drop-btn-open');
     };
 
     function showHideMenu() {
